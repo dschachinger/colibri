e2feb0382f1efcb87586fce5389d1ed3f0de6468
PratyushTalreja <pratyush.talreja@gmail.com>
2016-08-15 21:40:51 +0530

    Improved the Message header for Colibri Server

--- colibri-simulink/Col/src/main/java/com/colibri/Header/Header.java
+++ colibri-simulink/Col/src/main/java/com/colibri/Header/Header.java
@@ -1,119 +1,35 @@
-package com.colibri.Header;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.colibri.message.Header;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
 
 /**
- * Created by codelife on 12/8/16.
+ *
+ * @author codelife
  */
 public class Header {
 
     private String id;
-    private ContentType contentType;
     private Date date;
-    private String refenceId;
-    private Date expires;
-    private String headerAsString;
-    private static final String newLine = "<br>";
-
-    protected Header(String headerAsString) {
-        this.headerAsString = headerAsString;
-    }
-
-    protected Header(ContentType contentType) {
-        this.id = UUID.randomUUID().toString();
-        this.id = id.replace("-", "");
-        this.contentType = contentType;
-        this.date = new Date();
-    }
-
-    protected Header(ContentType contentType, String refenceId) {
-        this.id = UUID.randomUUID().toString();
-        this.id = id.replace("-", "");
-        this.contentType = contentType;
-        this.date = new Date();
-        this.refenceId = refenceId;
-    }
-
-    public Header(String id, ContentType contentType, Date date, Date expires) {
-        this.id = id;
-        this.contentType = contentType;
-        this.date = date;
-        this.expires = expires;
-    }
-
-    public Header(String id, ContentType contentType, Date date, String refenceId, Date expires) {
-        this.id = id;
-        this.contentType = contentType;
-        this.date = date;
-        this.refenceId = refenceId;
-        this.expires = expires;
-    }
-    public String getId() {
+    
+    public static String getId() {
+        String id = UUID.randomUUID().toString();
+        id = id.replace("-", "");
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public static String getDate() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+	Date date = new Date();
+        return dateFormat.format(date);
     }
 
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public String getMessageHeaderAsString() {
-        headerAsString = "";
-        if (this.hasDate()) {
-            headerAsString += "Date: " + TimeConverter.date2Ical(this.getDate()) + newLine;
-        }
-        headerAsString += "Content-Type: " + this.getContentType().getType() + newLine
-                + "Message-Id: " + this.getId() + newLine;
-        if (this.hasExpires()) {
-            headerAsString += "Expires: " + TimeConverter.date2Ical(this.getExpires()) + newLine;
-        }
-        if (this.hasReferenceId()) {
-            headerAsString += "Reference-Id: " + this.getRefenceId();
-        }
-        return headerAsString;
-    }
-
-    public String getRefenceId() {
-        return refenceId;
-    }
-
-    public void setRefenceId(String refenceId) {
-        this.refenceId = refenceId;
-    }
-
-    public Boolean hasReferenceId() {
-        return refenceId != null;
-    }
-
-    public Boolean hasDate() {
-        return date != null;
-    }
-
-    public ContentType getContentType() {
-        return contentType;
-    }
-
-    public void setContentType(ContentType contentType) {
-        this.contentType = contentType;
-    }
-
-    public Date getExpires() {
-        return expires;
-    }
-
-    public void setExpires(Date expires) {
-        this.expires = expires;
-    }
-
-    public Boolean hasExpires() {
-        return expires != null;
-    }
 }
+
--- colibri-simulink/Col/src/main/java/com/colibri/server/Colibri.java
+++ colibri-simulink/Col/src/main/java/com/colibri/server/Colibri.java
@@ -1,13 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.colibri.server;
 
-/**
- * Created by codelife on 12/8/16.
- */
-import com.colibri.Header.ContentType;
-import com.colibri.Header.Identifier;
-import com.colibri.Header.StatusCodes;
-import com.colibri.validator.Validate;
-
+import com.colibri.message.Header.ContentType;
+import com.colibri.message.Header.Header;
+import com.colibri.message.Header.Identifier;
+import com.colibri.message.Header.StatusCodes;
+import com.colibri.message.Validator.Validate;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.StringTokenizer;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonWriter;
@@ -16,12 +25,6 @@ import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
-import java.io.IOException;
-import java.io.StringWriter;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
 
 /**
  *
@@ -48,35 +51,16 @@ public class Colibri {
     @OnMessage
     public void handleMessage (String message, Session userSession) throws IOException
     {
+        String msg_id = "";
         //String username = (String) userSession.getUserProperties().get("username");
-        if (message.equalsIgnoreCase("STA"))
+        if (message.equalsIgnoreCase("DRE"))
         {
             if (tc.equalsIgnoreCase("TC"))
             {
-                String msg;
-                msg = Identifier.STA + "<br>Content-Type: " + ContentType.TEXT_PLAIN + "<br>Message-Id: random2<br>Reference-Id: random1<br>" + StatusCodes.OK;
-                if (sta.equalsIgnoreCase(""))
-                {
-                    breakStatus(msg);
-                    sta = "sta";
-                }
-                else
-                    breakStatus("Error: Cannot send Status message at this moment");
-            }
-            else
-                breakStatus("Error: Cannot send the Status as Technology Connecter is not registered");
-        }
-        else if (message.equalsIgnoreCase("DRE"))
-        {
-            if (tc.equalsIgnoreCase("TC"))
-            {
-                String msg;
-                msg = Identifier.DRE + "<br>Content-Type: " + ContentType.TEXT_PLAIN + "<br>Message-Id: random3<br>http://www.colibri-samples.org/TC";
-                breakStatus(msg);
-                tc = "";
-                String msgs;
-                msgs = Identifier.STA + "<br>Content-Type: " + ContentType.TEXT_PLAIN + "<br>Message-Id: random2<br>Reference-Id: random3<br>" + StatusCodes.OK;
-                breakStatus(msgs);
+            String msg;
+            msg = Identifier.DRE + "<br>Content-Type:" + ContentType.TEXT_PLAIN + "<br>Message-Id:" + Header.getId() + "<br>Date:"+ Header.getDate() +"<br>http://www.colibri-samples.org/TC"+ "<br>";
+            breakStatus(msg);
+            tc = "";
             }
             else
                 breakStatus("Error: Cannot Deregister the connector as Technology Connecter is not registered");
@@ -85,16 +69,16 @@ public class Colibri {
         {
             if (tc.equalsIgnoreCase("TC"))
             {
-                String msg;
-                msg = Identifier.REM + "<br>Content-Type: " + ContentType.TEXT_PLAIN + "<br>Message-Id: random5<br>http://www.colibri-samples.org/temperatureservice";
-                if (addt.equalsIgnoreCase("addt"))
-                {
-                    breakStatus(msg);
-                    addt = "";
-                    sta = "";
-                }
-                else
-                    breakStatus("Error: Cannot remove service as service is not added");
+            String msg;
+            msg = Identifier.REM + "<br>Content-Type:" + ContentType.TEXT_PLAIN + "<br>Message-Id:" + Header.getId() + "<br>Date:"+ Header.getDate() +"<br>http://www.colibri-samples.org/temperatureservice"+ "<br>";
+            if (addt.equalsIgnoreCase("addt"))
+            {
+                breakStatus(msg);
+                addt = "";
+                sta = "";
+            }
+            else
+                breakStatus("Error: Cannot remove service as service is not added");
             }
             else
                 breakStatus("Error: Cannot Remove service as Technology Connecter is not registered");
@@ -103,16 +87,16 @@ public class Colibri {
         {
             if (tc.equalsIgnoreCase("TC"))
             {
-                String msg;
-                msg = "OBST<br>" + Identifier.OBS + "<br>Content-Type: " + ContentType.TEXT_PLAIN + "<br>Message-Id: random6<br>http://www.colibri-samples.org/temperatureservice";
-                if (addt.equalsIgnoreCase("addt"))
-                {
-                    breakStatus(msg);
-                    obst = "obst";
-                    sta = "";
-                }
-                else
-                    breakStatus("Error: Cannot observe service as service is not added");
+            String msg;
+            msg = "OBST<br>" + Identifier.OBS + "<br>Content-Type:" + ContentType.TEXT_PLAIN + "<br>Message-Id:" + Header.getId() + "<br>Date:"+ Header.getDate() +"<br>http://www.colibri-samples.org/temperatureservice"+ "<br>";
+            if (addt.equalsIgnoreCase("addt"))
+            {
+                breakStatus(msg);
+                obst = "obst";
+                sta = "";
+            }
+            else
+                breakStatus("Error: Cannot observe service as service is not added");
             }
             else
                 breakStatus("Error: Cannot Observe service as Technology Connecter is not registered");
@@ -121,16 +105,16 @@ public class Colibri {
         {
             if (tc.equalsIgnoreCase("TC"))
             {
-                String msg;
-                msg = Identifier.DET + "<br>Content-Type: " + ContentType.TEXT_PLAIN + "<br>Message-Id: random7<br>http://www.colibri-samples.org/temperatureservice";
-                if (obst.equalsIgnoreCase("obst") && addt.equalsIgnoreCase("addt"))
-                {
-                    breakStatus(msg);
-                    obst = "";
-                    sta = "";
-                }
-                else
-                    breakStatus("Error: Cannot dettach service as service is not observed");
+            String msg;
+            msg = Identifier.DET + "<br>Content-Type:" + ContentType.TEXT_PLAIN + "<br>Message-Id:" + Header.getId() + "<br>Date:"+ Header.getDate() +"<br>http://www.colibri-samples.org/temperatureservice"+ "<br>";
+            if (obst.equalsIgnoreCase("obst") && addt.equalsIgnoreCase("addt"))
+            {
+                breakStatus(msg);
+                obst = "";
+                sta = "";
+            }
+            else
+                breakStatus("Error: Cannot dettach service as service is not observed");
             }
             else
                 breakStatus("Error: Cannot Dettach service as Technology Connecter is not registered");
@@ -139,14 +123,14 @@ public class Colibri {
         {
             if (tc.contentEquals("TC"))
             {
-                if (addt.equalsIgnoreCase("addt"))
-                {
-                    breakStatus("GETT<br>GET<br>Content-Type: text/plain<br>Message-Id: random12<br>http://www.colibri-samples.org/temperatureservice");
-                    obst = "";
-                    sta = "";
-                }
-                else
-                    breakStatus("Error: Cannot get the service as service is not added");
+            if (addt.equalsIgnoreCase("addt"))
+            {
+                breakStatus("GETT<br>GET" + "<br>Content-Type:" + ContentType.TEXT_PLAIN + "<br>Message-Id:" + Header.getId() + "<br>Date:"+ Header.getDate() +"<br>http://www.colibri-samples.org/temperatureservice"+ "<br>");
+                obst = "";
+                sta = "";
+            }
+            else
+                breakStatus("Error: Cannot get the service as service is not added");
             }
             else
                 breakStatus("Error: Cannot Get the service as Technology Connecter is not registered");
@@ -155,16 +139,16 @@ public class Colibri {
         {
             if (tc.equalsIgnoreCase("TC"))
             {
-                String msg;
-                msg = Identifier.REM + "<br>Content-Type: " + ContentType.TEXT_PLAIN + "<br>Message-Id: random14<br>http://www.colibri-samples.org/lightservice";
-                if (addl.equalsIgnoreCase("addl"))
-                {
-                    breakStatus(msg);
-                    addl = "";
-                    sta = "";
-                }
-                else
-                    breakStatus("Error: Cannot remove service as service is not added");
+            String msg;
+            msg = Identifier.REM + "<br>Content-Type:" + ContentType.TEXT_PLAIN + "<br>Message-Id:" + Header.getId() + "<br>Date:"+ Header.getDate() +"<br>http://www.colibri-samples.org/lightservice"+ "<br>";
+            if (addl.equalsIgnoreCase("addl"))
+            {
+                breakStatus(msg);
+                addl = "";
+                sta = "";
+            }
+            else
+                breakStatus("Error: Cannot remove service as service is not added");
             }
             else
                 breakStatus("Error: Cannot Remove service as Technology Connecter is not registered");
@@ -173,16 +157,16 @@ public class Colibri {
         {
             if (tc.equalsIgnoreCase("TC"))
             {
-                String msg;
-                msg = "OBSL<br>" + Identifier.OBS + "<br>Content-Type: " + ContentType.TEXT_PLAIN + "<br>Message-Id: random15<br>http://www.colibri-samples.org/lightservice";
-                if (addl.equalsIgnoreCase("addl"))
-                {
-                    breakStatus(msg);
-                    obsl = "obsl";
-                    sta = "";
-                }
-                else
-                    breakStatus("Error: Cannot observe service as service is not added");
+            String msg;
+            msg = "OBSL<br>" + Identifier.OBS + "<br>Content-Type:" + ContentType.TEXT_PLAIN + "<br>Message-Id:" + Header.getId() + "<br>Date:"+ Header.getDate() +"<br>http://www.colibri-samples.org/lightservice"+ "<br>";
+            if (addl.equalsIgnoreCase("addl"))
+            {
+                breakStatus(msg);
+                obsl = "obsl";
+                sta = "";
+            }
+            else
+                breakStatus("Error: Cannot observe service as service is not added");
             }
             else
                 breakStatus("Error: Cannot Observe service as Technology Connecter is not registered");
@@ -191,16 +175,16 @@ public class Colibri {
         {
             if (tc.equalsIgnoreCase("TC"))
             {
-                String msg;
-                msg = Identifier.DET + "<br>Content-Type: " + ContentType.TEXT_PLAIN + "<br>Message-Id: random16<br>http://www.colibri-samples.org/lightservice";
-                if (obsl.equalsIgnoreCase("obsl") && addl.equalsIgnoreCase("addl"))
-                {
-                    breakStatus(msg);
-                    obsl = "";
-                    sta = "";
-                }
-                else
-                    breakStatus("Error: Cannot dettach service as service is not observed");
+            String msg;
+            msg = Identifier.DET + "<br>Content-Type:" + ContentType.TEXT_PLAIN + "<br>Message-Id:" + Header.getId() + "<br>Date:"+ Header.getDate() +"<br>http://www.colibri-samples.org/lightservice"+ "<br>";
+            if (obsl.equalsIgnoreCase("obsl") && addl.equalsIgnoreCase("addl"))
+            {
+                breakStatus(msg);
+                obsl = "";
+                sta = "";
+            }
+            else
+                breakStatus("Error: Cannot dettach service as service is not observed");
             }
             else
                 breakStatus("Error: Cannot Dettach service as Technology Connecter is not registered");
@@ -209,14 +193,14 @@ public class Colibri {
         {
             if (tc.equalsIgnoreCase("TC"))
             {
-                if (addl.equalsIgnoreCase("addl"))
-                {
-                    breakStatus("GETL<br>GET<br>Content-Type: text/plain<br>Message-Id: random17<br>http://www.colibri-samples.org/lightservice");
-                    obsl = "";
-                    sta = "";
-                }
-                else
-                    breakStatus("Error: Cannot get the service as service is not added");
+            if (addl.equalsIgnoreCase("addl"))
+            {
+                breakStatus("GETL<br>GET" + "<br>Content-Type:" + ContentType.TEXT_PLAIN + "<br>Message-Id:" + Header.getId() + "<br>Date:"+ Header.getDate() +"<br>http://www.colibri-samples.org/lightservice"+ "<br>");
+                obsl = "";
+                sta = "";
+            }
+            else
+                breakStatus("Error: Cannot get the service as service is not added");
             }
             else
                 breakStatus("Error: Cannot Get the service as Technology Connecter is not registered");
@@ -225,25 +209,33 @@ public class Colibri {
         {
             if (tc.equalsIgnoreCase("TC"))
             {
-                if (que.equalsIgnoreCase("QUE") && qre.equalsIgnoreCase(""))
-                {
-                    String msg;
-                    msg = Identifier.QRE + "<br>Content-Type: " + ContentType.APPLICATION_RESULT + "<br>Message-Id: random26<br>Reference-Id: random25<br>";
-                    msg += "{<br>\"head\": { \"vars\": [\"service\"] },<br>\"results\": {<br>\"bindings\": [<br>{<br>\"service\" : { \"type\": \"uri\", \"value\": \"http://www.colibri-samples.org/service1\" },<br>\"identifier\" : { \"type\": \"literal\", \"value\": \"temp_monitoring_17\" }<br>}<br>]<br>}<br>}";
-                    breakStatus(msg);
-                }
-                else
-                    breakStatus("Error: Result without query");
+            if (que.equalsIgnoreCase("QUE") && qre.equalsIgnoreCase(""))
+            {
+                String msg;
+                msg = Identifier.QRE + "<br>Content-Type: " + ContentType.APPLICATION_RESULT + "<br>Message-Id: random26<br>Reference-Id: random25<br>";
+                msg += "{<br>\"head\": { \"vars\": [\"service\"] },<br>\"results\": {<br>\"bindings\": [<br>{<br>\"service\" : { \"type\": \"uri\", \"value\": \"http://www.colibri-samples.org/service1\" },<br>\"identifier\" : { \"type\": \"literal\", \"value\": \"temp_monitoring_17\" }<br>}<br>]<br>}<br>}";
+                breakStatus(msg);
+            }
+            else
+                breakStatus("Error: Result without query");
             }
             else
                 breakStatus("Error: Cannot send QRE result as Technology Connecter is not registered");
         }
         else
         {
-            String[] lines = message.split("<br>");
+        String[] lines = message.split("<br>");
             for(String ss:lines)
             {
-                if (ss.equalsIgnoreCase("REG"))
+                if (ss.contains("Message-Id:"))
+                {
+                    StringTokenizer st =  new StringTokenizer(ss,"Message-Id:");
+                    while (st.hasMoreTokens())
+                    {
+                        msg_id = st.nextToken();
+                    }
+                }
+                if (ss.equals("REG"))
                 {
                     temp = "REG";
                     tc = "TC";
@@ -256,30 +248,13 @@ public class Colibri {
                     addt = "addt";
                 if (ss.contains("lightservice") && addl.equalsIgnoreCase(""))
                     addl = "addl";
-                //System.out.println(ss);
-        /*if (username == null)
-        {
-            userSession.getUserProperties().put("username", ss);
-            userSession.getBasicRemote().sendText(buildJSONData("System","You are now connected as "+message));
-        }
-        else
-        {*/
-                Iterator<Session> iterator = chatUsers.iterator();
-                while (iterator.hasNext())
-                    iterator.next().getBasicRemote().sendText(buildJSONData(ss));
-                //}
+            Iterator<Session> iterator = chatUsers.iterator();
+            while (iterator.hasNext())
+                iterator.next().getBasicRemote().sendText(buildJSONData(ss));
             }
-            /*if (temp.equalsIgnoreCase("REG"))
-            {
-                temp = "";
-                Iterator<Session> iterator = chatUsers.iterator();
-                while (iterator.hasNext())
-                {
-                    breakStatus("STA<br>Content-Type: text/plain<br>Message-Id: random2<br>Reference-Id: random1<br>200 OK");
-                    //iterator.next().getBasicRemote().sendText(Message.getStatus());
-                }
-            }*/
-        }
+    }
+        String status = sta(msg_id);
+        breakStatus(status);
     }
     @OnClose
     public void handleClose (Session userSession)
@@ -296,13 +271,19 @@ public class Colibri {
 
     private void breakStatus(String message) throws IOException {
         String[] lines = message.split("<br>");
-        for(String ss:lines)
-        {
-            Iterator<Session> iterator = chatUsers.iterator();
-            while (iterator.hasNext())
+            for(String ss:lines)
             {
+                Iterator<Session> iterator = chatUsers.iterator();
+                while (iterator.hasNext())
+                {
                 iterator.next().getBasicRemote().sendText(buildJSONData(ss));
+                }
             }
-        }
+    }
+
+    private String sta(String msg_id) {
+        String msg = "";
+        msg = Identifier.STA + "<br>Content-Type:" + ContentType.TEXT_PLAIN + "<br>Message-Id:" + Header.getId() + "<br>Reference-Id:" + msg_id + "<br>Date:" + Header.getDate() + "<br>" +StatusCodes.OK + "<br>";
+        return msg;
     }
 }
