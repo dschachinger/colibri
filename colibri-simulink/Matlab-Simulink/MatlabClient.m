c0a7cd2b42b662cb153f5a90b3b8dacab0000f4f
PratyushTalreja <pratyush.talreja@gmail.com>
2016-08-22 14:52:36 +0530

    Update MatlabClient.m

--- colibri-simulink/Matlab-Simulink/MatlabClient.m
+++ colibri-simulink/Matlab-Simulink/MatlabClient.m
@@ -1,13 +1,18 @@
 import java.io.*;
 import java.net.*;
 try
-    A = 1;
-    temp = 25;
-    lightstatus = 'off';
-    tl = 0;
-    tx = 0;
-    tc = now;
-    set_param(bdroot,'SimulationCommand','start');
+    A = 1; 
+    %% A is the message code that comes from the connector. Default is set to 1 so that the simulink runs properly. 
+    %% A = 1 ---> Get the temperature value
+    %% A = 2 ---> Switch on the light
+    %% A = 3 ---> Switch off the light
+    %% A = 4 ---> Get the light status
+    temp = 25; % Default temperature value is set to 25
+    lightstatus = 'off'; % At the start of the simulation, light is off and it changes based upon the value of A = 2 or A = 3
+    tl = 0; % Stores the time when A = 2 is called to check for the increase in temperature
+    tx = 0; % Stores the time when A = 3 is called to check for the decrease in temperature
+    tc = now; % Stores the current time when A = 1 is called
+    set_param(bdroot,'SimulationCommand','start'); % Starts the simulation
     host = 'localhost';
     port = 8888;
     address = InetAddress.getByName(host);
@@ -33,7 +38,7 @@ try
             pause(3);
             block = 'models/To Workspace';
             rto = get_param(block, 'RuntimeObject'); % This gets the value during runtime
-            t = rto.InputPort(1).Data;
+            t = rto.InputPort(1).Data; % t gets the dynamic value
             disp(t);
             bw.println(num2str(t)); % Send the value to the connector
         case 2 % This means switch on the light
@@ -44,7 +49,7 @@ try
             pause(3);
             block = 'models/To Workspace1';
             rto = get_param(block, 'RuntimeObject');
-            t = rto.InputPort(1).Data;
+            t = rto.InputPort(1).Data; % t gets the dynamic value
 	    lightstatus = 'on';
             disp(t);
             bw.println(t); % Send the light status back to the Connector
@@ -58,7 +63,7 @@ try
             block = 'models/To Workspace2';
             rto = get_param(block, 'RuntimeObject');
             disp(rto.InputPort(1).Data);
-            t = rto.InputPort(1).Data;
+            t = rto.InputPort(1).Data; % t gets the dynamic value
             bw.println(t); % Send the light status back to the connector
         case 4 % This means get the light status
             bw.println(lightstatus);
